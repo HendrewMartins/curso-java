76b8bdfec82a058a5daa4150cd3d6812e525f8ee
HendrewMartins <hendrewmartins@hotmail.com>
2022-05-29 20:44:49 -0300

    ajuste Menssage

--- src/main/java/br/cursojava/exception/MenssageNotFoundException.java
+++ src/main/java/br/cursojava/exception/MenssageNotFoundException.java
@@ -1,11 +1,9 @@
 package br.cursojava.exception;
 
-public class MenssageNotFoundException extends Exception{
+public class MenssageNotFoundException extends Exception {
 
-    private static final Long serialVersionUID = 1L;
-
-    public MenssageNotFoundException(String message){
+    public MenssageNotFoundException(String message) {
         super(message);
     }
-    
+
 }
